6e6877dbac80e844bce2303abeb35c60ea32370a
Shaun Davis <davissp14@Shauns-MacBook-Pro.local>
2021-11-22 19:22:13 -0600

    Configuring users

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -10,7 +11,10 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/fly-apps/postgres-standalone/pkg/flypg"
+	"github.com/fly-apps/postgres-standalone/pkg/flypg/admin"
 	"github.com/fly-apps/postgres-standalone/pkg/supervisor"
+	"github.com/pkg/errors"
 )
 
 func main() {
@@ -24,6 +28,23 @@ func main() {
 	if err := setDefaultHBA(); err != nil {
 		PanicHandler(err)
 	}
+
+	go func() {
+		t := time.NewTicker(1 * time.Second)
+		defer t.Stop()
+		fmt.Println("Configuring users")
+
+		for range t.C {
+			if err := createRequiredUsers(); err != nil {
+				fmt.Println("Failed to create require users... retrying...")
+				continue
+			}
+
+			fmt.Println("Successfully created users")
+
+			return
+		}
+	}()
 
 	svisor := supervisor.New("flypg", 5*time.Minute)
 	svisor.AddProcess("flypg", "gosu postgres postgres -D /data/postgres/")
@@ -41,7 +62,7 @@ func main() {
 func initializePostgres() {
 	fmt.Println("Initializing Postgres")
 
-	if err := ioutil.WriteFile("/data/.default_password", []byte(os.Getenv("PGPASSWORD")), 0644); err != nil {
+	if err := ioutil.WriteFile("/data/.default_password", []byte(os.Getenv("OPERATOR_PASSWORD")), 0644); err != nil {
 		PanicHandler(err)
 	}
 
@@ -135,6 +156,59 @@ func setDefaultHBA() error {
 	}
 
 	return nil
+}
+
+func createRequiredUsers() error {
+	fmt.Println("Creating required users")
+
+	node, err := flypg.NewNode()
+	if err != nil {
+		return err
+	}
+
+	conn, err := node.NewLocalConnection(context.TODO())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Connection: %+v\n", conn)
+
+	curUsers, err := admin.ListUsers(context.TODO(), conn)
+	if err != nil {
+		return errors.Wrap(err, "failed to list current users")
+	}
+
+	credMap := map[string]string{
+		"flypgadmin": os.Getenv("SU_PASSWORD"),
+	}
+
+	for user, pass := range credMap {
+		exists := false
+		for _, curUser := range curUsers {
+			if user == curUser.Username {
+				exists = true
+			}
+		}
+		var sql string
+
+		if exists {
+			sql = fmt.Sprintf("ALTER USER %s WITH PASSWORD '%s'", user, pass)
+		} else {
+			// create user
+			switch user {
+			case "flypgadmin":
+				fmt.Println("Creating flypgadmin")
+				sql = fmt.Sprintf(`CREATE USER %s WITH SUPERUSER LOGIN PASSWORD '%s'`, user, pass)
+			}
+
+			_, err := conn.Exec(context.Background(), sql)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+
 }
 
 func PanicHandler(err error) {
--- pkg/flypg/node.go
+++ pkg/flypg/node.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"net/url"
 	"os"
 	"strconv"
 	"time"
@@ -14,45 +13,27 @@ import (
 	"github.com/pkg/errors"
 )
 
-const (
-	BackendStoreConsul      = "consul"
-	BackendStoreEtcd        = "etcdv3"
-	BackendStoreUnspecified = ""
-)
-
 type Credentials struct {
 	Username string
 	Password string
 }
 
 type Node struct {
-	AppName       string
-	PrivateIP     net.IP
-	Region        string
-	PrimaryRegion string
-	DataDir       string
-
+	AppName             string
+	PrivateIP           net.IP
+	Region              string
+	DataDir             string
 	SUCredentials       Credentials
-	ReplCredentials     Credentials
 	OperatorCredentials Credentials
-
-	BackendStore    string
-	BackendStoreURL *url.URL
-
-	KeeperUID string
-	StoreNode string
-
-	PGPort      int
-	PGProxyPort int
+	PGPort              int
 }
 
 func NewNode() (*Node, error) {
 	node := &Node{
-		AppName:     "local",
-		PGPort:      5433,
-		PGProxyPort: 5432,
-		Region:      "local",
-		DataDir:     "/data",
+		AppName: "local",
+		PGPort:  5432,
+		Region:  "local",
+		DataDir: "/data",
 	}
 
 	if region := os.Getenv("FLY_REGION"); region != "" {
@@ -74,44 +55,42 @@ func NewNode() (*Node, error) {
 		node.PGPort = port
 	}
 
+	node.SUCredentials = Credentials{
+		Username: "flypgadmin",
+		Password: os.Getenv("SU_PASSWORD"),
+	}
+
+	node.OperatorCredentials = Credentials{
+		Username: "postgres",
+		Password: os.Getenv("OPERATOR_PASSWORD"),
+	}
+
 	return node, nil
-}
-
-func openConnection(ctx context.Context, hosts []string, mode string, creds Credentials) (*pgx.Conn, error) {
-	if mode == "" {
-		mode = "any"
-	}
-
-	result := make(chan *pgx.Conn, len(hosts))
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	for _, host := range hosts {
-		url := fmt.Sprintf("postgres://%s/postgres?target_session_attrs=%s", host, mode)
-		conf, err := pgx.ParseConfig(url)
-		if err != nil {
-			return nil, err
-		}
-		conf.User = creds.Username
-		conf.Password = creds.Password
-		conf.ConnectTimeout = 5 * time.Second
-
-		go func() {
-			if cnn, err := pgx.ConnectConfig(ctx, conf); err == nil {
-				result <- cnn
-			}
-		}()
-	}
-
-	select {
-	case cnn := <-result:
-		return cnn, nil
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	}
 }
 
 func (n *Node) NewLocalConnection(ctx context.Context) (*pgx.Conn, error) {
 	host := net.JoinHostPort(n.PrivateIP.String(), strconv.Itoa(n.PGPort))
-	return openConnection(ctx, []string{host}, "any", n.SUCredentials)
+	return openConnection(ctx, host, n.OperatorCredentials)
+}
+
+func openConnection(ctx context.Context, host string, creds Credentials) (*pgx.Conn, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	url := fmt.Sprintf("postgres://%s/postgres", host)
+	conf, err := pgx.ParseConfig(url)
+	if err != nil {
+		return nil, err
+	}
+
+	conf.User = creds.Username
+	conf.Password = creds.Password
+	conf.ConnectTimeout = 5 * time.Second
+
+	conn, err := pgx.ConnectConfig(ctx, conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
