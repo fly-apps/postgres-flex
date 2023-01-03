8f541c95fb41e7e9e35e23623d139ce1edc19fe8
Shaun Davis <davissp14@gmail.com>
2023-01-03 16:23:12 -0600

    Cleanup

--- cmd/failover_validation/main.go
+++ cmd/failover_validation/main.go
@@ -7,23 +7,20 @@ import (
 )
 
 func main() {
-	nodeID := flag.Int("node-id", 0, "the node id")
-	nodeName := flag.String("node-name", "", "the name of the proposed leader")
-	visibleNodes := flag.Int("visable-nodes", 0, "Total visible nodes from the perspective of the proposed leader")
+	visibleNodes := flag.Int("visible-nodes", 0, "Total visible nodes from the perspective of the proposed leader")
 	totalNodes := flag.Int("total-nodes", 0, "The total number of nodes registered")
 	flag.Parse()
-
-	fmt.Printf("Verifying failover candidate %d: %s\n", nodeID, *nodeName)
 
 	// If there are no visible nodes, then we can't accept leadership as we are not able to
 	// confirm a network partition.
 	if *visibleNodes == 0 {
-		fmt.Println("Zero visible nodes detected. Enabling read-only mode.")
+		fmt.Println("Zero visible nodes detected.")
 		os.Exit(1)
 	}
 
-	// TODO - This will remove HA from a 2-node cluster setup until we can workout how to
-	// differentiate between a down node and a network partition.
+	// TODO - This will ultimately remove HA from a 2-node cluster setup.
+	// This will be the case until we come up with a strategy to differentiate
+	// between a down node and a network partition.
 
 	// We have visible nodes, but not enough to meet quorum.
 	if *visibleNodes < (*totalNodes/2 + 1) {
