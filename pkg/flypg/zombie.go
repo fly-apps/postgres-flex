00a743d1289b1128d311a421f3284bcdd7b2b3a8
Shaun Davis <davissp14@gmail.com>
2023-01-27 21:57:23 -0600

    Cleanup

--- pkg/flypg/zombie.go
+++ pkg/flypg/zombie.go
@@ -42,64 +42,73 @@ func readZombieLock() (string, error) {
 
 var ErrZombieDiscovered = errors.New("Zombie")
 
+// ZombieDiagnosis takes information about the current cluster state and does two things.
+// 1. Determines whether or not we are a primary coming back from the dead.
+// 2. If we are indeed a zombie, we want to see if we can resolve who the real primary is.
 func ZombieDiagnosis(myHostname string, total int, inactive int, active int, conflictMap map[string]int) (string, error) {
-	// No standbys
+	// Single node cluster
 	if total == 1 {
 		return myHostname, nil
 	}
 
-	// If all standbys are inactive
-	if total == (inactive - 1) {
-		// Possible network split
-		return "", ErrZombieDiscovered
-	}
-
+	// Two node setup
 	if total == 2 {
-		if len(conflictMap) > 0 || inactive > 0 {
+		// If the one standby is inactive we can will not be able to determine a down node from a net-split.
+		// If the standby reports a different primary we have diverged greatly.
+		if len(conflictMap) > 0 || inactive == 1 {
 			return "", ErrZombieDiscovered
 		}
 
 		return myHostname, nil
 	}
 
-	// Quorum can be met so long the majority of the registered members agree on who's the primary.
+	// If all standbys are inactive, then there's a possible network split.
+	if total == (inactive - 1) {
+		return "", ErrZombieDiscovered
+	}
+
 	quorum := ((total)/2 + 1)
+
+	// If there's not enough active nodes to meet quorum we have to assume
+	// we are a zombie.
 	if active < quorum {
 		fmt.Printf("Active: %d, Quorum: %d\n", active, quorum)
 		return "", ErrZombieDiscovered
 	}
 
-	// If we can meet quorum and there's no conflicts, we can boot as primary.
+	// We can safely say we are primary if we have enough active nodes
+	// to meet quorum and there are no conflicts.
 	if len(conflictMap) == 0 && active >= quorum {
 		return myHostname, nil
 	}
 
-	highMember := ""
-	highCount := 0
+	topCandidate := ""
+	highestCount := 0
 
 	totalConflicts := 0
 
+	// Calculate total conflicts + highest reported primary
 	for hostname, total := range conflictMap {
 		totalConflicts += total
 
-		if total > highCount {
-			highCount = total
-			highMember = hostname
+		if total > highestCount {
+			highestCount = total
+			topCandidate = hostname
 		}
 	}
 
+	// We can infer our count by subtracting inactive members and total conficts.
 	myCount := total - inactive - totalConflicts
 
-	if myCount > highCount {
-		highCount = myCount
-		highMember = myHostname
+	if myCount > highestCount {
+		highestCount = myCount
+		topCandidate = myHostname
 	}
 
-	fmt.Printf("Hcount: %d, Quorum: %d\n", highCount, quorum)
-	if highCount < quorum {
+	if highestCount < quorum {
 		// Unable to reach quorum
 		return "", ErrZombieDiscovered
 	}
 
-	return highMember, nil
+	return topCandidate, nil
 }
