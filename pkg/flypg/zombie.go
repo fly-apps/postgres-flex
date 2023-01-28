53201ca2ece1aac5087f79f59d37e9d76fd33e6e
Shaun Davis <davissp14@gmail.com>
2023-01-28 15:40:32 -0600

    Simplify diagnosis logic

--- pkg/flypg/zombie.go
+++ pkg/flypg/zombie.go
@@ -48,58 +48,36 @@ func ZombieDiagnosis(myHostname string, total int, inactive int, active int, con
 		return myHostname, nil
 	}
 
-	// Two node cluster
-	if total == 2 {
-		// We can't know if we are the true primary if our only standby is inactive or has conflicts.
-		if len(conflictMap) > 0 || inactive == 1 {
-			return "", ErrZombieDiscovered
-		}
+	quorum := total/2 + 1
 
-		return myHostname, nil
-	}
-
-	// If all standbys are inactive, then we hae to assume a possible network split.
-	if total == (inactive - 1) {
-		return "", ErrZombieDiscovered
-	}
-
-	quorum := (total/2 + 1)
-
-	// if we have enough active nodes to meet quorum and there are no conflicts we are the primary.
-	if len(conflictMap) == 0 && active >= quorum {
-		return myHostname, nil
-	}
-
+	// Active members must at least equal quorum in order for primary resolution to be succcessful.
 	if active < quorum {
 		return "", ErrZombieDiscovered
 	}
 
 	topCandidate := ""
-	highestCount := 0
+	highestTotal := 0
 	totalConflicts := 0
 
 	// Evaluate conflicts to calculate the highest referrenced primary
 	for hostname, total := range conflictMap {
 		totalConflicts += total
 
-		if total > highestCount {
-			highestCount = total
+		if total > highestTotal {
+			highestTotal = total
 			topCandidate = hostname
 		}
 	}
 
-	// Calculate our count
 	myCount := total - inactive - totalConflicts
 
-	// If we meet quorum, we are done here.
 	if myCount >= quorum {
 		return myHostname, nil
 	}
 
-	// If our highest
-	if highestCount < quorum {
-		return "", ErrZombieDiscovered
+	if highestTotal >= quorum {
+		return topCandidate, ErrZombieDiscovered
 	}
 
-	return topCandidate, ErrZombieDiscovered
+	return "", ErrZombieDiscovered
 }
