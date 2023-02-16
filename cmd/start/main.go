df6a74b9f8f95a133e4175b25e7a0e139c68bbaf
Shaun Davis <davissp14@gmail.com>
2023-02-16 09:56:38 -0600

    Fix haproxy cmd

--- cmd/start/main.go
+++ cmd/start/main.go
@@ -53,7 +53,7 @@ func main() {
 		"PRIMARY_REGION":    os.Getenv("PRIMARY_REGION"),
 		"PG_LISTEN_ADDRESS": node.PrivateIP,
 	}
-	svisor.AddProcess("proxy", "/etc/init.d/haproxy -W -db -f /fly/haproxy.cfg", supervisor.WithEnv(proxyEnv), supervisor.WithRestart(0, 1*time.Second))
+	svisor.AddProcess("proxy", "/usr/sbin/haproxy -W -db -f /fly/haproxy.cfg", supervisor.WithEnv(proxyEnv), supervisor.WithRestart(0, 1*time.Second))
 
 	svisor.AddProcess("repmgrd", fmt.Sprintf("gosu postgres repmgrd -f %s --daemonize=false", node.RepMgr.ConfigPath),
 		supervisor.WithRestart(0, 5*time.Second),
