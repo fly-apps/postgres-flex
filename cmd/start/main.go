7a62edea666574680fa0fc0523a114cdb6792c24
swilson-fly <123485058+swilson-fly@users.noreply.github.com>
2024-04-08 16:11:58 -0400

    Update main.go to fix scale to zero functionality (#225)
    Currently we're checking if the number of connections is greater-or-equal to 1. However the ConnectionCount query that runs doesn't exclude itself from the result set, so it always returns at least one. 
    
    The >= 1 check was added here: https://github.com/fly-apps/postgres-flex/pull/200. This PR reverts back to the previous `>1 ` check.

--- cmd/start/main.go
+++ cmd/start/main.go
@@ -154,7 +154,7 @@ func scaleToZeroWorker(ctx context.Context, node *flypg.Node) error {
 				continue
 			}
 			log.Printf("Current connection count is %d\n", current)
-			if current >= 1 {
+			if current > 1 {
 				continue
 			}
 			return fmt.Errorf("scale to zero condition hit")
